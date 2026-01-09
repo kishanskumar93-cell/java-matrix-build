3d44d1ba5ffdf2dba89032b355aff22658256398
kishanskumar93-cell <kishanskumar93@gmail.com>
2026-01-09 10:36:28 +0530

    Create App class with main method for output

---
+++ src/main/java/app.java
@@ -0,0 +1,7 @@
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("✅ Java Matrix Build Successful!");
+        System.out.println("Running on Java version: " + System.getProperty("java.version"));
+    }
+}
